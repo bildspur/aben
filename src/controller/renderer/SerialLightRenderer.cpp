ef6b5345d14b0747d9871e60dce30d0c339fbd8c
Florian Bruggisser <florian@bildspur.ch>
2018-11-21 21:23:42 +0100

    implemented serial renderer fix

--- src/controller/renderer/SerialLightRenderer.cpp
+++ src/controller/renderer/SerialLightRenderer.cpp
@@ -24,7 +24,7 @@ void SerialLightRenderer::loop() {
 
         for (int i = 0; i < installation->getSize(); i++) {
             // todo: fix!
-            auto brightness = mapToGlobalBrightnessRange(0.0f);//installation->getPortal(i)->getBrightness());
+            auto brightness = mapToGlobalBrightnessRange(installation->getPortal(i)->getLed()->getBrightness());
             msg += String(brightness) + ";";
         }
 
