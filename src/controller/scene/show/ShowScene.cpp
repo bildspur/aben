af22a5e2327c16674eef660a80ab99cfb59c5c84
Florian Bruggisser <florian@bildspur.ch>
2018-12-10 02:46:18 +0100

    implemented basic example scene

--- src/controller/scene/show/ShowScene.cpp
+++ src/controller/scene/show/ShowScene.cpp
@@ -43,33 +43,27 @@ void ShowScene::reset() {
 
 void ShowScene::setupKeyPoints() {
     keyPoints.clear();
-    keyPoints.push_back(KeyPointSet<PORTAL_SIZE>(0.0f, RGBColor::BLACK()));
-
-    /*
-    for (int i = 0; i < 20; i++) {
-        auto on = MathUtils::isRandomCalled(0.8);
-
-        if (i % 2 == 0)
-            keyPoints.emplace_back(0.2f, RGBColor::WHITE());
-        else
-            keyPoints.emplace_back(0.2f, RGBColor::BLACK());
-    }
-     */
-
-    keyPoints.push_back(KeyPointSet<PORTAL_SIZE>(0.0f, RGBColor::BLACK()));
+    keyPoints.emplace_back(0.0f, RGBColor::BLACK());
 
     // start
-    keyPoints.push_back(KeyPointSet<PORTAL_SIZE>(0.5f, 0, RGBColor::BLACK()));
-    keyPoints.push_back(KeyPointSet<PORTAL_SIZE>(0.5f, 1, RGBColor::BLACK()));
-    keyPoints.push_back(KeyPointSet<PORTAL_SIZE>(0.5f, 2, RGBColor::BLACK()));
-    keyPoints.push_back(KeyPointSet<PORTAL_SIZE>(0.5f, 3, RGBColor::BLACK()));
+    keyPoints.emplace_back(0.5f, 0, RGBColor::BLACK());
+    keyPoints.emplace_back(0.5f, 1, RGBColor::BLACK());
+    keyPoints.emplace_back(0.5f, 2, RGBColor::BLACK());
+    keyPoints.emplace_back(0.5f, 3, RGBColor::BLACK());
+    keyPoints.emplace_back(0.5f, 4, RGBColor::BLACK());
 
     // end
-    keyPoints.push_back(KeyPointSet<PORTAL_SIZE>(0.5f, 0, RGBColor::WHITE()));
-    keyPoints.push_back(KeyPointSet<PORTAL_SIZE>(0.5f, 1, RGBColor::WHITE()));
-    keyPoints.push_back(KeyPointSet<PORTAL_SIZE>(0.5f, 2, RGBColor::WHITE()));
-    keyPoints.push_back(KeyPointSet<PORTAL_SIZE>(0.5f, 3, RGBColor::WHITE()));
+    keyPoints.emplace_back(0.5f, 0, RGBColor::WHITE());
+    keyPoints.emplace_back(0.5f, 1, RGBColor::WHITE());
+    keyPoints.emplace_back(0.5f, 2, RGBColor::WHITE());
+    keyPoints.emplace_back(0.5f, 3, RGBColor::WHITE());
+    keyPoints.emplace_back(0.5f, 4, RGBColor::WHITE());
 
-    keyPoints.push_back(KeyPointSet<PORTAL_SIZE>(1.0f, RGBColor::BLACK()));
-    keyPoints.push_back(KeyPointSet<PORTAL_SIZE>(1.0f, RGBColor::BLACK()));
+    // flash
+    for (int i = 0; i < 50; i++) {
+        keyPoints.emplace_back(0.1f, i % 2 == 0 ? RGBColor::WHITE() : RGBColor::BLACK());
+    }
+
+    keyPoints.emplace_back(1.0f, RGBColor::BLACK());
+    keyPoints.emplace_back(1.0f, RGBColor::BLACK());
 }
