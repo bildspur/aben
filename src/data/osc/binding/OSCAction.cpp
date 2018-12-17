9cbdfb988631b2dfde574fb197e8b11a45a0285b
Florian Bruggisser <florian@bildspur.ch>
2018-12-18 00:13:49 +0100

    implemented action rule

--- src/data/osc/binding/OSCAction.cpp
+++ src/data/osc/binding/OSCAction.cpp
@@ -1,5 +1,20 @@
+#include <utility>
+
 //
 // Created by Florian Bruggisser on 17.12.18.
 //
 
 #include "OSCAction.h"
+
+OSCAction::OSCAction(String address, std::function<void(IOSCPublisher *publisher)> f)
+        : OSCRule(std::move(address)), function(std::move(f)) {
+
+}
+
+void OSCAction::receive(IOSCPublisher *publisher, OSCMessage &msg) {
+    function(publisher);
+}
+
+void OSCAction::publish(IOSCPublisher *publisher) {
+    // do nothing
+}
--- src/data/osc/binding/OSCAction.h
+++ src/data/osc/binding/OSCAction.h
@@ -1,3 +1,5 @@
+#include <utility>
+
 //
 // Created by Florian Bruggisser on 17.12.18.
 //
@@ -6,8 +8,21 @@
 #define ABEN_OSCACTION_H
 
 
-class OSCAction {
+#include <functional>
+#include <WString.h>
+#include "OSCRule.h"
 
+class OSCAction : public OSCRule {
+    // based on: https://stackoverflow.com/a/9186537/1138326
+
+    std::function<void(IOSCPublisher *publisher)> function;
+
+public:
+    explicit OSCAction(String address, std::function<void(IOSCPublisher *publisher)> f);
+
+    void receive(IOSCPublisher *publisher, OSCMessage &msg) override;
+
+    void publish(IOSCPublisher *publisher) override;
 };
 
 
